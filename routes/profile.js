ded5c0b6feb74672ff4024b56051cf1e725b9d40
Nizam Salim <nizam.salim7@gmail.com>
2021-10-15 17:54:33 +0530

    minor changes to route

--- routes/profile.js
+++ routes/profile.js
@@ -1,31 +1,31 @@
-const { createProfile,getProfile,deleteProfile,updateProfile }= require('../controller/profile');
+const { createProfile,getProfile,deleteProfile }= require('../controller/profile');
 const getUser =require("../middleware/getUser");
 const router=require('express').Router();
 
 //route-api/profile/createprofile
 //type-post
-//acess-private
+//access-private
 router.post('/createProfile',getUser,createProfile);
 
 
 
 //route-api/profile/getprofile/
 //type-get
-//acess-private
-router.get('/getProfile/',getUser,getProfile);
+//access-private
+router.get('/getProfile',getUser,getProfile);
 
 
 /*
 //route-api/profile/updateProfile
 //type-post
-//acess-private
+//access-private
 router.post('/updateProfile',getUser,updateProfile);
 */
 
 
 //route-api/profile/deleteProfile
 //type-delete
-//acess-private
+//access-private
 router.delete('/deleteProfile',getUser,deleteProfile);
 
 module.exports=router;
